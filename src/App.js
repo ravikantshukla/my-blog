580676ef1d8ebee6dba224dfb5d3e6b1866c0cf7
= <=>
2022-12-24 11:18:05 +0530

    Router implementation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ArticlesListPage from './pages/ArticlesListPage';
 import ArticlePage from './pages/ArticlePage';
+import NavBar from './navBar';
+
 import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>My Perfect Blog</h1>
-      <HomePage/>
-      <AboutPage/>
-      <ArticlesListPage/>
-      <ArticlePage/>
-      <NotFoundPage/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <NavBar/>
+        <h1>My Perfect Blog</h1>
+        <div id='page-id'>
+          <Routes>
+            <Route path='/' element={<HomePage/>}/>
+            <Route path='/about' element={<AboutPage/>}/>
+            <Route path='/articles' element={<ArticlesListPage/>}/>
+            <Route path='/articles/:articled' element={<ArticlePage/>}/>
+            
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
