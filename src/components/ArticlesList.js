4b9ee166ebfb0515d7c81cb42127637ca766535d
= <=>
2022-12-24 23:27:56 +0530

    added article list component

---
+++ src/components/ArticlesList.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+const ArticlesList = ({articles}) => {
+    return (
+        <>
+        {
+            articles.map(article => (
+                <Link key={article.name} to={`/articles/${article.name}`}>
+                    <h3>{article.title}</h3>
+                    <p>{article.content[0].substring(0,3)}</p>
+                
+                </Link>   
+            ))
+        
+        }
+        </>
+    )
+}
+
+export default ArticlesList;
--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -1,18 +1,12 @@
 import articles  from "./article-cotent";
-import {Link} from "react-router-dom";
+import ArticlesList from "../components/ArticlesList";
+
 
 const ArticlesListPage = () => {
     return (
         <>
         <h1>Articles</h1>
-        {
-            articles.map(article => (
-                <Link to={`/articles/${article.name}`}>
-                    <h3>{article.title}</h3>
-                    <p>{article.content[0].substring(0,3)}</p>
-                
-                </Link>   
-        ))}
+        <ArticlesList articles={articles}/>
         </>
     )
 }
